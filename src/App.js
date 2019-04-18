dc7c16f0ea6a639f39d93a35ed971ea2685634b0
Piotr Niełacny <piotr.nielacny@toptal.com>
2019-04-18 23:35:37 +0200

    Add simple layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Swift from 'swift-mock';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div>
+              <Navbar bg="dark" variant="dark">
+                  <Navbar.Brand href="#home">
+                      {'Swift Validator'}
+                  </Navbar.Brand>
+              </Navbar>
+              <Container>
+                  <Row>
+                      <Col>
+                          <Form>
+                              <Form.Group controlId="exampleForm.ControlTextarea1">
+                                  <Form.Control placeholder="Swift order" as="textarea" rows="20" />
+                              </Form.Group>
+                          </Form>
+                      </Col>
+                      <Col>
+                          <Form>
+                              <Form.Group controlId="exampleForm.ControlTextarea1">
+                                  <Form.Control placeholder="Swift transaction" as="textarea" rows="20" />
+                              </Form.Group>
+                          </Form>
+                      </Col>
+                  </Row>
+                  <Row>
+                      <Col>1 of 3</Col>
+                      <Col>2 of 3</Col>
+                  </Row>
+              </Container>
+          </div>
+        );
+    }
 }
 
 export default App;
